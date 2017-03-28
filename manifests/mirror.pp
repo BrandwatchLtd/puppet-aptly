02abf87f3866e1d809ecfbbca5e255966ee2d9ed
Ben Ashmead <bena@brandwatch.com>
2017-03-28 10:38:40 +0100

    was using the wrong var name :/

--- manifests/mirror.pp
+++ manifests/mirror.pp
@@ -87,6 +87,12 @@ define aptly::mirror (
     $components_arg = " ${components}"
   }
 
+  if $nondebian {
+    $sync_args = "${title} ${components_arg}"
+  } else {
+    $sync_args = "${title}"
+  }
+
   if $key {
     if is_array($key) {
       $key_string = join($key, "' '")
@@ -123,11 +129,6 @@ define aptly::mirror (
   }
 
   if $sync {
-    if $nondebian {
-      $sync_args = "${title} ${component_args}"
-    } else {
-      $sync_args = "${title}"
-    }
     include cron
     $minute = fqdn_rand(60, $title)
     cron::job {
